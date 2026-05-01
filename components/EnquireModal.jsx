2206757058643bdea305e76e744a2d283fc2fc5d
vineet-proptiger <vineet.mishra@proptiger.com>
2026-05-01 12:29:55 +0530

    some timing fixes

--- components/EnquireModal.jsx
+++ components/EnquireModal.jsx
@@ -17,8 +17,8 @@ const EnquireModal = ({ isOpen, setIsOpen }) => {
     const initial = setTimeout(() => {
       autoTriggered.current = true
       setIsOpen(true)
-      intervalRef.current = setInterval(() => setIsOpen(true), 20000)
-    }, 10000)
+      intervalRef.current = setInterval(() => setIsOpen(true), 30000)
+    }, 30000)
     return () => {
       clearTimeout(initial)
       if (intervalRef.current) clearInterval(intervalRef.current)
